79aa75ea4659acba04c01c0411b2d7e3d14f89c7
teena <grgteenu@gmail.com>
2020-06-25 17:54:13 +0530

    values from google sheet are converted to data frame

--- quickstart.py
+++ quickstart.py
@@ -4,6 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import pandas as pd
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
@@ -46,7 +47,8 @@ def main():
     if not values:
         print('No data found.')
     else:
-        print(values)
+        df=pd.DataFrame(values)
+        print(df)
         #for row in values:
             # Print columns A and E, which correspond to indices 0 and 4.
            # print('%s, %s' % (row[0], row[4]))
